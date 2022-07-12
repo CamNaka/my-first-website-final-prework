12c0c09bc68cebe5991b276763bfb52acfe6c8ec
Cameron <cameronnakaba4@gmail.com>
2022-07-11 18:10:06 -0700

    added bottom button

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.onscroll = () => {
     if((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
-        console.log("you are at end of page.")
     }
 }
+
+const toTop = () => window.scrollTo({top: 0, bahavior: 'smooth'});
